b3ebb2fa83713b7a62636a0e6aadff5ec4b5b7aa
feal93 <feal93@gmail.com>
2016-08-25 23:57:17 +0700

    Can run postag

--- ParseHTML/Handle/CRFProcess.cs
+++ ParseHTML/Handle/CRFProcess.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using java.io;
+using java.util;
+using edu.stanford.nlp.ling;
+using edu.stanford.nlp.tagger.maxent;
+using Console = System.Console;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+
 public class CRFProcess
 {
     public CRFProcess()
@@ -13,34 +14,58 @@ public class CRFProcess
     }
     public List<Word> doProcess(List<Word> lsWord)
     {
-        string output = string.Empty;
-        string error = string.Empty;
+        //string output = string.Empty;
+        //string error = string.Empty;
 
-        ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/c cd ../../CRF && crf_test -m model testdata.dat");
-        processStartInfo.RedirectStandardOutput = true;
-        processStartInfo.RedirectStandardError = true;
-        processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
-        processStartInfo.UseShellExecute = false;
+        //ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/c cd ../../CRF && crf_test -m model testdata.dat");
+        //processStartInfo.RedirectStandardOutput = true;
+        //processStartInfo.RedirectStandardError = true;
+        //processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
+        //processStartInfo.UseShellExecute = false;
 
-        Process process = Process.Start(processStartInfo);
+        //Process process = Process.Start(processStartInfo);
 
-        using (StreamReader streamReader = process.StandardOutput)
-        {
-            output = streamReader.ReadToEnd();
-        }
+        //using (StreamReader streamReader = process.StandardOutput)
+        //{
+        //    output = streamReader.ReadToEnd();
+        //}
 
-        using (StreamReader streamReader = process.StandardError)
-        {
-            error = streamReader.ReadToEnd();
-        }
+        //using (StreamReader streamReader = process.StandardError)
+        //{
+        //    error = streamReader.ReadToEnd();
+        //}
 
-        Console.WriteLine("The following output was detected:");
-        Console.WriteLine(output);
-        String[] lsLine = output.Split('\n');
-        foreach(String s in lsLine)
-        {
-            Console.WriteLine("=>"+s);
-        }
+        //Console.WriteLine("The following output was detected:");
+        //Console.WriteLine(output);
+        //String[] lsLine = output.Split('\n');
+        //foreach(String s in lsLine)
+        //{
+        //    Console.WriteLine("=>"+s);
+        //}
         return lsWord;
+    }
+    public List<Word> setPosTag(String text)
+    {
+        List<Word> lsW = new List<Word>();
+        var jarRoot = @"..\..\CRF\stanford-postagger-full-2015-12-09";
+        var modelsDirectory = jarRoot + @"\models";
+
+        // Loading POS Tagger
+        var tagger = new MaxentTagger(modelsDirectory + @"\wsj-0-18-bidirectional-nodistsim.tagger");
+
+        // Text for tagging
+        //text = "A Part-Of-Speech Tagger (POS Tagger) is a piece of software that reads text"
+        //           + "in some language and assigns parts of speech to each word (and other token),"
+        //           + " such as noun, verb, adjective, etc., although generally computational "
+        //           + "applications use more fine-grained POS tags like 'noun-plural'.";
+
+        var sentences = MaxentTagger.tokenizeText(new java.io.StringReader(text)).toArray();
+        Console.WriteLine("Set Pos Tag");
+        foreach (ArrayList sentence in sentences)
+        {
+            var taggedSentence = tagger.tagSentence(sentence);
+            Console.WriteLine(Sentence.listToString(taggedSentence, false));
+        }
+        return lsW;
     }
 }
